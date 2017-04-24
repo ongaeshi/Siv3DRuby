bde5968578d918329f5df0c28e702ca6f84c19b2
ongaeshi <ongaeshi0621@gmail.com>
2017-04-25 00:19:06 +0900

    Refine main.rb

--- Siv3DRuby/main.rb
+++ Siv3DRuby/main.rb
@@ -2,11 +2,9 @@ a = 1
 b = 3
 array = [1, 2, 3]
 # array.each { |e| }
+r = a + b + array[0] + array[1] + array[2]
 
-# a + b + array[0] + array[1] + array[2]
-
-println "Hello mruby #{1}"
-println "a"
+println "Hello mruby #{r}"
 println "こんにちは, Siv3DRuby"
 wait_key
 
