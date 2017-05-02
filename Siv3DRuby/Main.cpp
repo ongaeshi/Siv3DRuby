f54f7e91bab0f82d83cbfe5dc2c93fa1b2b67d44
ongaeshi <ongaeshi0621@gmail.com>
2017-05-02 23:04:26 +0900

    Format code

--- Siv3DRuby/Main.cpp
+++ Siv3DRuby/Main.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <Siv3D.hpp>
+﻿#include <Siv3D.hpp>
 #include "mruby.h"
 #include "mruby/compile.h"
 
@@ -7,15 +6,15 @@ extern void mrb_init(mrb_state *mrb);
 
 void Main()
 {
-	mrb_state* mrb = mrb_open();
-	mrb_init(mrb);
+    mrb_state* mrb = mrb_open();
+    mrb_init(mrb);
 
-	FILE* fp;
-	fopen_s(&fp, "main.rb", "r");
-	{
-		mrb_value ret = mrb_load_file(mrb, fp);
-	}
-	fclose(fp);
+    FILE* fp;
+    fopen_s(&fp, "main.rb", "r");
+    {
+        mrb_value ret = mrb_load_file(mrb, fp);
+    }
+    fclose(fp);
 
-	mrb_close(mrb);
+    mrb_close(mrb);
 }
